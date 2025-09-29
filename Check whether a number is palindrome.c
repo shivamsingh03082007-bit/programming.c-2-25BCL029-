8584897f259aab9aa54733b700c3aed1d6393b82
shivamsingh.github19 <shivamsingh03082007@gmail.com>
2025-09-30 01:12:56 +0530

    Create Check whether a number is palindrome.c

---
+++ Check whether a number is palindrome.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int num, reversed = 0, remainder, original;
+
+    
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    original = num; // Store original number for comparison
+
+    
+    while (num != 0) {
+        remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num /= 10;
+    }
+
+    
+    if (original == reversed)
+        printf("%d is a palindrome number.\n", original);
+    else
+        printf("%d is not a palindrome number.\n", original);
+
+    return 0;
+}
